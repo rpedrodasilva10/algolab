fd73d660a3b857ed94c79cbed6c8e37d909b6957
Renan Silva <renan.psilva@mercadolivre.com>
2023-10-09 21:38:49 -0300

    docs: add description, time and space complexity for IsPalindrome

--- palindrome.go
+++ palindrome.go
@@ -2,6 +2,10 @@ package algo
 
 import "strconv"
 
+// IsPalindrome is the brute force solution to check if a number is a palindrome
+// We run all items inverting the string and compare with the original value
+// Time complexity: O(n)
+// Space complexity: O(n)
 func IsPalindrome(x int) bool {
     strValue := strconv.Itoa(x)
     strArray := []rune(strValue)
